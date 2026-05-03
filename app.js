c28c332be872d1311df6075b73375cf179b42204
LucasLimaAZ <lucasdelimamonteiro@gmail.com>
2026-05-03 19:58:16 -0300

    Remove node_modules from repository

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require("express");
-
 const routes = require("./src/routes");
-
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
-
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(routes);
 
 module.exports = app;
